6a422ffea765d38d43ffac4e71071b586354898d
Yana <yanassam81@gmail.com>
2024-06-16 00:38:01 +0200

    bb

--- car-rental-app/vite.config.js
+++ car-rental-app/vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",
-  build: {
-    outDir: "dist",
-    rollupOptions: {
-      input: path.resolve(__dirname, "index.html"),
-    },
-  },
+  base: "/test-car/car-rental-app/",
 });
